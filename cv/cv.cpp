05c40ab080f90a973cb89e3efedbff4f5d4ae24f
Jiaran <haojiaran@gmail.com>
2014-03-30 23:12:59 -0400

    cv almost done

--- cv/cv.cpp
+++ cv/cv.cpp
@@ -1,6 +1,11 @@
 #include "cv.h"
+#include <stdlib.h>
 
+void drawSquare (int mx, int my, int x, int y , Mat img ){
+}
 
+void drawTriangle (int mx, int my, int x, int y , Mat img ){
+}
 
 void drawCircle(int mx, int my, int x, int y , Mat img ){
   int thickness = -1;
@@ -11,7 +16,7 @@ void drawCircle(int mx, int my, int x, int y , Mat img ){
   circle( img,
 	  center,
 	  r,
-	  Scalar( 255, 255, 255 ),
+	  Scalar( 255, 0, 0 ),
 	  thickness,
 	  lineType );  
 
@@ -24,4 +29,43 @@ Point getCenter(int mx, int my, int x, int y ){
   ans.x+= wide* (x-1);
   ans.y+= height*(y-1);
   return ans;
+}
+
+void drawRandom(int mx, int my, int x, int y , Mat img){
+  Point c = getCenter(mx,my,x,y);
+  int wide = WIDE/mx;
+  int height = HEIGHT/my;
+  for(int i= c.x-wide/2;i<c.x+wide/2;i++){
+    if(random(0.5)){
+      for(int j=c.y-height/2;j<c.y+height/2;j++){
+      
+	img.at<uchar>(j, i)=255;
+      }
+    }
+  }
+}
+
+
+bool random(double p){
+  double i = rand()%100+1;
+  return i<100*p;
+}
+
+
+int getRandomShape(int c, int s,int t ,int r ){
+  double i = rand()%100+1;
+  if(i<=c){
+    return CIRCLE;
+  }
+  else if(i<=s){
+    return SQUARE;
+  }
+  else if(i<=t){
+    return TRIANGLE;
+  }
+  else{
+    return RANDOM;
+  }
+  
+
 }
--- cv/cv.h
+++ cv/cv.h
@@ -10,10 +10,12 @@
 #define WIDE 400
 #define HEIGHT 400
 using namespace cv;
+enum {CIRCLE, SQUARE, TRIANGLE,RANDOM};
 void drawCircle(int mx, int my, int x, int y , Mat img);
 Point getCenter(int mx, int my, int x, int y);
 void drawTriangle(int mx, int my, int x, int y, Mat img);
 void drawRandom(int mx, int my, int x, int y , Mat img);
 void drawSquare(int mx, int my, int x, int y , Mat img);
-
+bool random(double p);
+int getRandomShape(int c, int s, int t, int r);
 #endif
