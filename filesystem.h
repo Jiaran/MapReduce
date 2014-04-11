aba79a81042a21e5bf1cd0f339d7a1554fed048a
wucangji <wucangji@gmail.com>
2014-04-11 00:32:51 -0400

    Revert 8bbaa96..422cd88
    This rolls back to commit 8bbaa96ad934e66d573dab15dc60528cd06ed97d.

--- filesystem.h
+++ filesystem.h
@@ -2,47 +2,10 @@
 
 #define FILESYSTEM_H
 
-
-
-#include <string>
-#include <iostream>
-#include <map>
-#include <stdlib.h>
-
-// class FileSystem{
-//  public:
-//   FileSystem();
+class FileSystem{
+ public:
+  FileSystem();
   
-// }
-
-//  communicate with Kang yi
-
-typedef struct image_info
-{
-	//cv::Mat image_mat; // not sure
-	int id;
-	string filename;	
-}image_info;
-
-
-class FileDir
-{
-protected:
-	// store the filepath of our file system;
-	char init_filepath[256];
-	map < int, string > mymap;
-
-public:
-	FileDir();
-	void Printfile();
-	void Createmap();
-	void Printmap();
-	//void PutinMap(int id, char* filename);
-	image_info* NumtoInfo(int id);
-
-};
-
-
-
+}
 
 #endif
