2e8bee98346ae12434c6e8e43e8c5b2e58ba9223
wucangji <wucangji@gmail.com>
2014-04-10 21:40:18 -0400

    dsfsf

--- filesystem.h
+++ filesystem.h
@@ -2,10 +2,47 @@
 
 #define FILESYSTEM_H
 
-class FileSystem{
- public:
-  FileSystem();
+
+
+#include <string>
+#include <iostream>
+#include <map>
+#include <stdlib.h>
+
+// class FileSystem{
+//  public:
+//   FileSystem();
   
-}
+// }
+
+//  communicate with Kang yi
+
+typedef struct image_info
+{
+	//cv::Mat image_mat; // not sure
+	int id;
+	string filename;	
+}image_info;
+
+
+class FileDir
+{
+protected:
+	// store the filepath of our file system;
+	char init_filepath[256];
+	map < int, string > mymap;
+
+public:
+	FileDir();
+	void Printfile();
+	void Createmap();
+	void Printmap();
+	//void PutinMap(int id, char* filename);
+	image_info* NumtoInfo(int id);
+
+};
+
+
+
 
 #endif
