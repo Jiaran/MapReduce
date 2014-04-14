fdd9fbaa655f51cfba9ae2b05ae070aa49272682
Kangyi1990 <zhangkangyi1990@gmail.com>
2014-04-13 22:43:26 -0400

    adsf

--- cv/cv.cpp
+++ cv/cv.cpp
@@ -1,10 +1,41 @@
 #include "cv.h"
 #include <stdlib.h>
 
-void drawSquare (int mx, int my, int x, int y , Mat img ){
+void drawSquare(int mx, int my, int x, int y , Mat img){
+	int wide = WIDE/mx;
+  	int height = HEIGHT/my;
+	int thickness = -1;
+  	int lineType = 8;
+	Point center=getCenter(mx,my,x,y);
+	Point corner=Point(center.x-(wide/2-5),center.y-(height/2-5));
+	Point oppsite=Point(corner.x+(wide-10),corner.y+(height-10));
+	rectangle(img,corner, oppsite, Scalar( 255, 255, 255 ),
+	  thickness,
+	  lineType);
 }
 
-void drawTriangle (int mx, int my, int x, int y , Mat img ){
+void drawTriangle(int mx, int my, int x, int y, Mat img){
+	int thickness = 1;
+  	int lineType = 8;
+	int wide = WIDE/mx;
+  	int height = HEIGHT/my;
+	Point center=getCenter(mx,my,x,y);
+
+  /** Create some points */
+  Point rook_points[1][3];
+  rook_points[0][0] = Point( center.x, center.y-(height/2-5) );
+  rook_points[0][1] = Point( center.x-(wide/2-5), center.y+(height/2-5) );
+  rook_points[0][2] = Point( center.x+(wide/2-5), center.y+(height/2-5) );
+
+  const Point* ppt[1] = { rook_points[0] };
+  int npt[] = { 3 };
+
+  fillPoly( img,
+            ppt,
+            npt,
+            thickness,
+            Scalar( 255, 255, 255 ),
+            lineType ); 
 }
 
 void drawCircle(int mx, int my, int x, int y , Mat img ){
